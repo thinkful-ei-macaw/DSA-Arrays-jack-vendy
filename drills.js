f9c3b50f9ac40ba47e537665e81dec68edbafae7
Jack Lansing <jack.lansing@live.com>
2020-05-05 00:09:00 -0400

    drills 9 and 10

--- drills.js
+++ drills.js
@@ -47,27 +47,69 @@ function filterNumsAlt(arr, num) {
 console.log(filterNums([5, 2, 2, 5, 4, 20, 5, 3, 10, 5], 5));
 
 /* Max sum in the array */
-function maxSum(array){
+function maxSum(array) {
   //keep track of current max
   let currentMax = 0;
   let total = 0;
-  for(let i = 0; i <array.length; i++){
-    total+=array[i];
-    if(currentMax < total){
+  for (let i = 0; i < array.length; i++) {
+    total += array[i];
+    if (currentMax < total) {
       currentMax = total;
     }
   }
   return currentMax;
 }
 
-console.log(maxSum([4,6,-3,5,-2,1]));
+console.log(maxSum([4, 6, -3, 5, -2, 1]));
 
 /* Merge Arrays */
-function mergeArray(arr1, arr2){
-
-  return [...arr1, ...arr2].sort((a,b)=>{
-    return a-b;
+function mergeArray(arr1, arr2) {
+  return [...arr1, ...arr2].sort((a, b) => {
+    return a - b;
   });
 }
 
-console.log('This is mergerArray', mergeArray([1, 3, 6, 8, 11], [2, 3, 5, 8, 9, 10]));
+console.log(
+  'This is mergerArray',
+  mergeArray([1, 3, 6, 8, 11], [2, 3, 5, 8, 9, 10]),
+);
+
+/* Remove characters */
+
+function removeChars(string, chars) {
+  if (!string.length) {
+    return '';
+  }
+
+  const re = new RegExp(`[${chars}]`, 'g');
+
+  if (re.test(string[0])) {
+    return removeChars(string.slice(1), chars);
+  } else return string[0] + removeChars(string.slice(1), chars);
+}
+
+let stringToRemoveFrom = 'Battle of the Vowels: Hawaii vs. Grozny';
+let charsToRemove = 'aeiou';
+
+console.log(removeChars(stringToRemoveFrom, charsToRemove));
+
+/* Products */
+
+function products(arr) {
+  const result = [];
+  let product = 1;
+
+  for (let i = 0; i < arr.length; i++) {
+    product = 1;
+
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j] !== arr[i]) {
+        product *= arr[j];
+      }
+    }
+    result.push(product);
+  }
+  return result;
+}
+
+console.log(products([1, 3, 9, 4]));
