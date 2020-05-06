b3570473387a41ced6dd42796ffb97013174a794
Vendy Prum <vendy@thinkful.com>
2020-05-06 12:56:11 -0700

    exercises 7 & 8

--- drills.js
+++ drills.js
@@ -45,3 +45,29 @@ function filterNumsAlt(arr, num) {
 }
 
 console.log(filterNums([5, 2, 2, 5, 4, 20, 5, 3, 10, 5], 5));
+
+/* Max sum in the array */
+function maxSum(array){
+  //keep track of current max
+  let currentMax = 0;
+  let total = 0;
+  for(let i = 0; i <array.length; i++){
+    total+=array[i];
+    if(currentMax < total){
+      currentMax = total;
+    }
+  }
+  return currentMax;
+}
+
+console.log(maxSum([4,6,-3,5,-2,1]));
+
+/* Merge Arrays */
+function mergeArray(arr1, arr2){
+
+  return [...arr1, ...arr2].sort((a,b)=>{
+    return a-b;
+  });
+}
+
+console.log('This is mergerArray', mergeArray([1, 3, 6, 8, 11], [2, 3, 5, 8, 9, 10]));
